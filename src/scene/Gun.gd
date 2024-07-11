fe32b85c7b23e57a0a1725a21fd0260fca9b74f0
Liko <782361105@qq.com>
2024-07-11 19:07:34 +0800

    修改代码文件目录

--- src/scene/Gun.gd
+++ src/scene/Gun.gd
@@ -1,21 +1,15 @@
 class_name Gun extends Marker2D
-## Represents a weapon that spawns and shoots bullets.
-## The Cooldown timer controls the cooldown duration between shots.
 
-
-const BULLET_SCENE = preload("res://resource/player/bullet.tscn")
-
-#@onready var sound_shoot := $Shoot as AudioStreamPlayer2D
 @onready var timer := $Cooldown as Timer
 
-@export var bullet_velocity := 500
+@export var bullet_scene : Resource
 
 
 # direction应指代方向，为单位向量
 func shoot(direction: Vector2 = Vector2(1,0)) -> bool:
 	if not timer.is_stopped():
 		return false
-	var bullet := BULLET_SCENE.instantiate() as Bullet
+	var bullet := bullet_scene.instantiate() as Bullet
 	bullet.global_position = global_position
 	bullet.direction = direction
 
